bd42f2df8f1170e5d03663337ad8aeaa03b5f210
David Schultz <davids24@gmail.com>
2021-07-19 16:38:26 -0500

    try switching to cached version of sigal

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 set -e
 SRC=${SRC:-albums}
 BUILD=${BUILD:-_build}
+
 sigal build --debug $SRC $BUILD $@
 python minify.py theme/static $BUILD/static
+
+chmod -R +r $BUILD
+find $BUILD -type d -exec chmod 2755 {} +
